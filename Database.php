ceff2b55146d1ddf488b5781c676901ca0119e58
dlee129 <david.y.lee09@gmail.com>
2021-07-18 17:14:14 -0400

    Commit#2: OOP Database class setup

---
+++ Database.php
@@ -0,0 +1,40 @@
+<?php
+
+class Database{
+  private $servername = "localhost";
+  private $database = "url";
+  private $username = "root";
+  private $password = "root";
+  private $table = "links ";
+  private $conn;
+
+  public function __construct(){
+    $conn = new mysqli($servername, $username, $password);
+    if($conn->connect_error){
+      die("Connection Failed: ". $conn->connect_error);
+    }
+    //create database if it doesnt exist
+    if($conn->select_db($database) === false){
+      if($result = $conn->query("CREATE DATABASE url;")) {
+
+      }
+    }
+    //select the database to work with
+    $conn->select_db($database);
+  }
+
+  protected function execute_query($sql){
+    $result = $conn->query($sql);
+    return $result;
+  }
+
+  public function get_rows($url){
+    $sql = "SELECT COUNT(*) FROM links WHERE url = $url";
+    $results = $this->execute_query($sql);
+    return $results;
+  }
+
+
+}//class bracket
+
+?>
